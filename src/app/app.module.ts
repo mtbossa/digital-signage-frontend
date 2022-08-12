277ca32f9e117a792689b9dd595dd68f56e6952a
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-08-11 21:36:55 -0300

    Add src code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,60 @@
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { HttpClientModule } from "@angular/common/http";
+import { APP_INITIALIZER, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { TuiAlertModule, TuiDialogModule, TuiRootModule } from "@taiga-ui/core";
+import { TUI_LANGUAGE, TUI_PORTUGUESE_LANGUAGE } from "@taiga-ui/i18n";
+import { CookieModule } from "ngx-cookie";
+import { of } from "rxjs";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppLayoutModule } from "./shared/feature/app-layout/app-layout.module";
+import { httpInterceptorProviders } from "./shared/interceptors";
+import { NotfoundComponent } from "./shared/ui/notfound/notfound.component";
+import { AuthService } from "./user/data-access/auth.service";
 
+function tryToGetUser(authService: AuthService) {
+  return () => {
+    return new Promise((resolve) => {
+      authService.fetchLoggedUser().subscribe({
+        next: (user) => {
+          authService.setLoggedUser(user);
+          resolve(user);
+        },
+        error: (err) => {
+          resolve(err);
+        },
+      });
+    });
+  };
+}
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NotfoundComponent],
   imports: [
+    CookieModule.withOptions(),
+    HttpClientModule,
     BrowserModule,
+    AppLayoutModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
     AppRoutingModule,
-      BrowserAnimationsModule,
-      TuiRootModule,
-      TuiDialogModule,
-      TuiAlertModule
-],
-  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
-  bootstrap: [AppComponent]
+  ],
+  providers: [
+    httpInterceptorProviders,
+    {
+      provide: TUI_LANGUAGE,
+      useValue: of(TUI_PORTUGUESE_LANGUAGE),
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: tryToGetUser,
+      deps: [AuthService],
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -0,0 +1,44 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpStatusCode,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { CookieService } from "ngx-cookie";
+import { catchError, Observable, throwError } from "rxjs";
+import { AuthService } from "src/app/user/data-access/auth.service";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(
+    private router: Router,
+    private cookieService: CookieService,
+    private authService: AuthService
+  ) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.cookieService.get("XSRF-TOKEN");
+
+    if (token) {
+      req = req.clone({
+        setHeaders: { "X-XSRF-TOKEN": token },
+      });
+    }
+
+    return next.handle(req.clone({ withCredentials: true })).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === HttpStatusCode.Unauthorized) {
+          this.authService.setLoggedUser(null);
+          this.router.navigateByUrl("/login");
+        }
+
+        return throwError(() => err);
+      })
+    );
+  }
+}
