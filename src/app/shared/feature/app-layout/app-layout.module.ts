1161e5e029bccd2d2144d12c5005da3f526b2fec
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-08-15 13:26:53 -0300

    Transform app-sidebar into standalone component

--- src/app/shared/feature/app-layout/app-layout.module.ts
+++ src/app/shared/feature/app-layout/app-layout.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
-import { AppSidebarModule } from "../../ui/app-sidebar/app-sidebar.module";
+import { AppSidebarComponent } from "../../ui/app-sidebar/app-sidebar.component";
 import { AppLayoutComponent } from "./app-layout.component";
 
 @NgModule({
   declarations: [AppLayoutComponent],
-  imports: [CommonModule, RouterModule, AppSidebarModule],
+  imports: [CommonModule, RouterModule, AppSidebarComponent],
   exports: [AppLayoutComponent],
 })
 export class AppLayoutModule {}
