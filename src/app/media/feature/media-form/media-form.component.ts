0345f1e5698bcc7b94d4a7508c710993b39e1827
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-08-25 20:39:55 -0300

    Start medias form

--- src/app/media/feature/media-form/media-form.component.ts
+++ src/app/media/feature/media-form/media-form.component.ts
@@ -2,6 +2,39 @@ import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { TuiDay, TuiTime } from "@taiga-ui/cdk";
+import {
+  TuiButtonModule,
+  TuiErrorModule,
+  TuiGroupModule,
+  TuiTextfieldControllerModule,
+} from "@taiga-ui/core";
+import {
+  TuiDataListWrapperModule,
+  TuiFieldErrorModule,
+  TuiFieldErrorPipeModule,
+  TuiInputDateModule,
+  TuiInputModule,
+  TuiInputSliderModule,
+  TuiSelectModule,
+  TuiStepperModule,
+} from "@taiga-ui/kit";
+
+class User {
+  constructor(readonly firstName: string, readonly lastName: string) {}
+
+  toString(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+class Account {
+  constructor(
+    readonly id: string,
+    readonly name: string,
+    readonly amount: number,
+    readonly cardSvg: string
+  ) {}
+}
 
 @Component({
   selector: `app-media-form`,
@@ -9,9 +42,33 @@ import { TuiDay, TuiTime } from "@taiga-ui/cdk";
   styleUrls: [`./media-form.component.scss`],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TuiStepperModule,
+    TuiFieldErrorPipeModule,
+    TuiFieldErrorModule,
+    TuiInputSliderModule,
+    TuiDataListWrapperModule,
+    TuiSelectModule,
+    TuiTextfieldControllerModule,
+    TuiErrorModule,
+    TuiGroupModule,
+    TuiButtonModule,
+    TuiInputModule,
+    TuiInputDateModule,
+  ],
 })
 export class MediaFormComponent {
+  readonly svgIcons = {
+    common: `https://ng-web-apis.github.io/dist/assets/images/common.svg`,
+    universal: `https://ng-web-apis.github.io/dist/assets/images/universal.svg`,
+    intersection: `https://ng-web-apis.github.io/dist/assets/images/intersection-observer.svg`,
+    mutation: `https://ng-web-apis.github.io/dist/assets/images/mutation-observer.svg`,
+  };
+
+  persons = [new User(`Roman`, `Sedov`), new User(`Alex`, `Inkin`)];
+
   testForm = new FormGroup({
     nameValue: new FormControl(``, Validators.required),
     textValue: new FormControl(``, Validators.required),
@@ -20,6 +77,7 @@ export class MediaFormComponent {
     moneyValue: new FormControl(`100`, Validators.required),
     periodValue: new FormControl(new TuiDay(2017, 2, 15), Validators.required),
     timeValue: new FormControl(new TuiTime(12, 30), Validators.required),
+    personValue: new FormControl(this.persons[0]),
     quantityValue: new FormControl(0, Validators.required),
     radioValue: new FormControl(`with-commission`),
     accountWherefrom: new FormControl(null),
