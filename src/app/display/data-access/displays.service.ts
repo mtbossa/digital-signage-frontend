078f4dda2d3465f599f66614612cd7daa5e56d08
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-08-31 18:51:59 -0300

    Show api token when creating display

--- src/app/display/data-access/displays.service.ts
+++ src/app/display/data-access/displays.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, take } from "rxjs";
+import { BehaviorSubject, Observable, take } from "rxjs";
 import { PaginatedResponse } from "src/app/shared/data-access/interfaces/PaginatedResponse.interface";
 import { environment } from "src/environments/environment";
 
@@ -17,6 +17,20 @@ export interface Display {
   store_id: number | null;
   created_at: string;
   updated_at: string;
+  token?: Token;
+}
+
+interface Token {
+  accessToken: {
+    abilities: Array<string>;
+    created_at: string;
+    id: number;
+    name: string;
+    tokenable_id: number;
+    tokenable_type: string;
+    updated_at: string;
+  };
+  plainTextToken: string;
 }
 
 export type Key = "id" | "name" | "width" | "height" | "size" | "touch";
@@ -30,20 +44,21 @@ export class DisplaysService {
   private displays$ = new BehaviorSubject<Display[] | null>(null);
 
   public fetchIndex(key: Key, direction: -1 | 1, page: number, size: number) {
-    return this.http.get<PaginatedResponse<Display>>(
-      `${environment.apiUrl}/api/displays`,
-      {
+    return this.http
+      .get<PaginatedResponse<Display>>(`${environment.apiUrl}/api/displays`, {
         params: {
           page,
           size,
         },
-      }
-    );
+      })
+      .pipe(take(1));
   }
 
   // Needs to be FormData since we need to upload the file
   public create(data: ValidDisplayForm) {
-    return this.http.post(`${environment.apiUrl}/api/displays`, data).pipe(take(1));
+    return this.http
+      .post<Display>(`${environment.apiUrl}/api/displays`, data)
+      .pipe(take(1));
   }
 
   public show(displayId: number) {
@@ -64,6 +79,8 @@ export class DisplaysService {
   }
 
   public remove(displayId: number) {
-    return this.http.delete(`${environment.apiUrl}/api/displays/${displayId}`);
+    return this.http
+      .delete(`${environment.apiUrl}/api/displays/${displayId}`)
+      .pipe(take(1));
   }
 }
--- src/app/display/feature/display-create-form/display-create-form.component.ts
+++ src/app/display/feature/display-create-form/display-create-form.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from "@angular/common";
 import { HttpClient } from "@angular/common/http";
 import { Component, Inject, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { TuiAlertService, TuiNotification } from "@taiga-ui/core";
+import { TuiAlertService, TuiDialogService, TuiNotification } from "@taiga-ui/core";
 import { environment } from "src/environments/environment";
 
 import { DisplaysService } from "../../data-access/displays.service";
@@ -21,6 +21,8 @@ import {
 export class DisplayCreateFormComponent {
   constructor(
     @Inject(TuiAlertService) private readonly alertService: TuiAlertService,
+    @Inject(TuiDialogService)
+    private readonly dialogService: TuiDialogService,
     private route: Router,
     private displaysService: DisplaysService
   ) {}
@@ -31,6 +33,13 @@ export class DisplayCreateFormComponent {
         this.route.navigate(["../displays"]);
         this.alertService
           .open(`Display criado com sucesso!`, { status: TuiNotification.Success })
+          .subscribe();
+        this.dialogService
+          .open(`Anote o token do Display: ${res.token?.plainTextToken}`, {
+            label: `Token da API`,
+            size: `s`,
+            data: { button: `Fechar` },
+          })
           .subscribe();
       },
     });
