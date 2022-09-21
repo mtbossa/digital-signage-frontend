dce3178f625fe4febf2ac02c88dd100b0b78b8cc
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-09-21 09:23:16 -0300

    Create ServerErrorInterceptor

--- src/app/shared/data-access/interceptors/index.ts
+++ src/app/shared/data-access/interceptors/index.ts
@@ -2,6 +2,7 @@ import { HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { CSRFInterceptor } from "./csrf.interceptor";
 import { NotFoundInterceptor } from "./not-found.interceptor";
+import { ServerErrorInterceptor } from "./server-error.interceptor";
 import { UnauthorizedInterceptor } from "./unauthorized.interceptor";
 
 /** Http interceptor providers in outside-in order */
@@ -9,4 +10,5 @@ export const appHttpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: CSRFInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: UnauthorizedInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: NotFoundInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: ServerErrorInterceptor, multi: true },
 ];
